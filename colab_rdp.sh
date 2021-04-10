67e5a376f2f43b6f6bb72d43e1f44f83e3800540
CyberNari <75901966+cybernari@users.noreply.github.com>
2021-04-11 05:17:33 +0530

    Update colab_rdp.sh

--- colab_rdp.sh
+++ colab_rdp.sh
@@ -18,6 +18,8 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
+sudo apt-get update -y
+sudo apt-get install -y xfce4-terminal
 sudo adduser cybernari chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
